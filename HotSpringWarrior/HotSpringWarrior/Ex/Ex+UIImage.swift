89e1bf25590eec51ca37e0c8029d41fb0f55b549
tomoshigewakita <>
2024-09-06 23:38:45 +0900

    ユーザの移動軌跡の可視化

---
+++ HotSpringWarrior/HotSpringWarrior/Ex/Ex+UIImage.swift
@@ -0,0 +1,26 @@
+//
+//  Ex+UIImage.swift
+//  HotSpringWarrior
+//
+//  Created by tomoshigewakita on 2024/09/06.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+
+        let resizedSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+
+        UIGraphicsBeginImageContext(resizedSize)
+        draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return resizedImage
+    }
+}
